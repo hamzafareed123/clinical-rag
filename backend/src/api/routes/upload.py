13e619fb16e4e7671d0c17f34c07aea76427e900
Hamza <hamzafareed616@gmail.com>
2026-07-03 04:38:51 +0500

    file validation

--- backend/src/api/routes/upload.py
+++ backend/src/api/routes/upload.py
@@ -1,4 +1,4 @@
-from fastapi import UploadFile, APIRouter, Form
+from fastapi import UploadFile, APIRouter, Form, HTTPException
 from src.core.config import settings
 from src.services.ingestor import ingest_document
 import os
@@ -13,6 +13,12 @@ os.makedirs(settings.UPLOAD_DIR, exist_ok=True)
 @router.post("/post-file")
 async def post_file(file: UploadFile, session_id: str = Form(...)):
 
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="Only PDF is allowed")
+
+    if not file.size > 10_000_000:
+        raise HTTPException(status_code=400, detail="File size must be under 10MB")
+    
     file_path = os.path.join(settings.UPLOAD_DIR, file.filename)
 
     collection_name = f"session_{session_id}"
